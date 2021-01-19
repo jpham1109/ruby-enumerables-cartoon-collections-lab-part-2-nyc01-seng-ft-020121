0429774f70c5a038eff985a404ea097734df8ba7
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-19 01:56:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,8 @@ def find_valid_calls(planeteer_calls)
   result = nil
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |call|
-    valid_calls.include?
+    valid_calls.include?(call)
+    result = call
+  end 
+  result
 end
